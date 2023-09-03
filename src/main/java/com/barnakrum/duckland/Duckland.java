41be55c0d297dc709b9ac658255a38537530fa5c
Barnakrum <krzysiu03@tlen.pl>
2023-09-03 19:32:22 +0200

    Initial commit

---
+++ src/main/java/com/barnakrum/duckland/Duckland.java
@@ -0,0 +1,15 @@
+package com.barnakrum.duckland;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Duckland implements ModInitializer {
+	public static final String MOD_ID = "duckland";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+	}
+}
